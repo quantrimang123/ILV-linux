98c89733ac4c8b56806f23a5470ce6c180400082
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-06-24 23:06:55 +0700

    Update profiledef.sh

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -14,6 +14,7 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/usr/local/bin/ilv-install.sh"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"

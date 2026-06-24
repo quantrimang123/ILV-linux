9e542fd09bc3ba23deee7bef586604a0551c4fa5
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-06-25 05:11:45 +0700

    Add user input for installation configuration
    Prompt user for username and password, update configuration file accordingly, and use temporary config for installation.

--- configs/releng/airootfs/root/ilv-install.sh
+++ configs/releng/airootfs/root/ilv-install.sh
@@ -7,15 +7,21 @@ if ! ping -c 1 archlinux.org &> /dev/null; then
     echo "Không có internet! Vui lòng dùng 'nmtui' để kết nối trước."
     exit 1
 fi
-
+read -p "Nhập username: " input_user
+read -s -p "Nhập mật khẩu: " input_pass
+echo ""
+jq --arg user "$input_user" --arg pass "$input_pass" \
+   '.users[0].username = $user | .users[0].password = $pass' \
+   ilv_config.json > ilv_config_tmp.json
+   
+echo "Đang khởi tạo cấu hình..."
 echo "Đang khởi động tiến trình cài đặt tự động ILV-LINUX..."
-archinstall --config /root/ilv_config.json
-
+archinstall --config ilv_config_tmp.json
 if [ $? -eq 0 ]; then
     touch /etc/ilv_installed
     echo "Cài đặt thành công! File flag đã được tạo."
 fi
-
+rm ilv_config_tmp.json
 echo "------------------------------------------"
 echo "Cài đặt xong! Hãy khởi động lại máy."
 echo "------------------------------------------"

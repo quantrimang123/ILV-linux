b946a6efc877ff2539db0dfc81a02cd90536de3d
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-06-25 09:40:12 +0700

    Update profiledef.sh

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -10,7 +10,7 @@ bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-b' '256K' '-Xdict-size' '256K')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:600"

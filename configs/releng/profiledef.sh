9ee6c97514870dd62aebbedcd474b075aba958cb
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-06-25 07:18:20 +0700

    chore(config): tighten /etc/shadow perms to 600 and set /etc/sudoers.d to 755

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -13,7 +13,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
+  ["/etc/shadow"]="0:0:600"
   ["/root/ilv-install.sh"]="0:0:755"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
@@ -22,6 +22,6 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/skel/.bashrc"]="0:0:644"
-  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:755"
   ["/etc/NetworkManager/dispatcher.d/09-timezone"]="0:0:755"
 )

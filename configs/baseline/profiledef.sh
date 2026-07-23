2ff2aedacb5a9c16c1886434bb5c047cd1a75450
quantrimang123 <121271358+quantrimang123@users.noreply.github.com>
2026-07-23 23:29:34 +0700

    Update profiledef.sh

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-baseline"
+iso_name="ILVLinux-baseline"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux baseline"
+iso_publisher="ILV Linux <hd070812@gmail.com>"
+iso_application="ILV Linux baseline"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
